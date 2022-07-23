b73d4035e37d59469559127de80173f7652a3ce6
Mr. Luka <info.tsiklauri@gmail.com>
2022-07-23 22:30:00 +0200

    comment page transition changed

--- lib/presentation/widgets/buttons/add_comment_button.dart
+++ lib/presentation/widgets/buttons/add_comment_button.dart
@@ -9,13 +9,8 @@ class AddCommentButton extends StatelessWidget {
     return FloatingActionButton(
       splashColor: Colors.white,
       backgroundColor: const Color.fromRGBO(246, 198, 152, 1),
-      onPressed: () async {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => const CommentPage(),
-          ),
-        );
+      onPressed: () {
+        Navigator.of(context).push(_VideoZoomInRoute());
       },
       child: const Icon(
         Icons.edit_note,
@@ -24,4 +19,24 @@ class AddCommentButton extends StatelessWidget {
       ),
     );
   }
+}
+
+Route _VideoZoomInRoute() {
+  return PageRouteBuilder(
+      pageBuilder: (context, animation, secondaryAnimation) =>
+          const CommentPage(),
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        const begin = Offset(0.0, 1.0);
+        const end = Offset.zero;
+        const curve = Curves.ease;
+
+        var tween =
+            Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+        return SlideTransition(
+          position: animation.drive(tween),
+          child: child,
+        );
+      },
+      transitionDuration: const Duration(milliseconds: 500));
 }
